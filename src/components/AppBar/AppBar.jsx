95a0e98d7c1e9a018f23f38e7e24a80517147c89
PetroPodolets <146197695+PetroPodolets@users.noreply.github.com>
2025-03-29 13:26:18 +0000

    update nav

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,29 +1,27 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import css from "./AppBar.module.css"
+import css from "./AppBar.module.css";
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 
-
-
 export default function AppBar() {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
-    return (
-        <header className={css.header}>
-            
-            <a href="#" className={css.logo}> <svg width="24" height="24">
-                <use href="../../../public/symbol-defs.svg#icon-logo"></use>
-                </svg> <p>VocabBuilder</p></a>
-               
-                
-            {isLoggedIn && (
-                <>
-                    
-                    <Navigation /> 
-                    <UserMenu />
-                </>
-                
-            )}
-        </header>
-    );
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return (
+    <header className={css.header}>
+      <a href="#" className={css.logo}>
+        {" "}
+        <svg width="24" height="24">
+          <use href="../../../public/symbol.svg#icon-logo"></use>
+        </svg>{" "}
+        <p>VocabBuilder</p>
+      </a>
+
+      {isLoggedIn && (
+        <>
+          <Navigation />
+          <UserMenu />
+        </>
+      )}
+    </header>
+  );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,41 +1,37 @@
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import css from "./Navigation.module.css"
+import css from "./Navigation.module.css";
 
 export default function Navigation() {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-    const getButtonStyle = (isActive) => ({
-        color: isActive ? 'white' : 'black',
-        fontWeight: isActive ? '500' : '85AA9F',
-        backgroundColor: isActive ? '#85AA9F' : 'transparent'
-        ,
-    });
+  return (
+    <nav className={css.nav}>
+      {isLoggedIn && (
+        <>
+          <NavLink
+            to="/dictionary"
+            className={({ isActive }) => (isActive ? css.active : css.inactive)}
+          >
+            <button className={css.button}>Dictionary</button>
+          </NavLink>
 
+          <NavLink
+            to="/recommend"
+            className={({ isActive }) => (isActive ? css.active : css.inactive)}
+          >
+            <button className={css.button}>Recommend</button>
+          </NavLink>
 
-    return (
-        <nav className={css.nav}>
-            {isLoggedIn && (<>
-                <NavLink to="/dictionary" className={({ isActive }) => (isActive ? css.active : "")}>
-                    {({ isActive }) => (
-                        <button style={getButtonStyle(isActive)}>Dictionary</button>
-                    )}
-                </NavLink>
-
-                <NavLink to="/recommend" className={({ isActive }) => (isActive ? css.active : "")}>
-                    {({ isActive }) => (
-                        <button style={getButtonStyle(isActive)}>Recommend</button>
-                    )}
-                </NavLink>
-
-                <NavLink to="/training" className={({ isActive }) => (isActive ? css.active : "")}>
-                    {({ isActive }) => (
-                        <button style={getButtonStyle(isActive)}>Training</button>
-                    )}
-                </NavLink>
-            </>
-            )}
-        </nav>
-    )
+          <NavLink
+            to="/training"
+            className={({ isActive }) => (isActive ? css.active : css.inactive)}
+          >
+            <button className={css.button}>Training</button>
+          </NavLink>
+        </>
+      )}
+    </nav>
+  );
 }
